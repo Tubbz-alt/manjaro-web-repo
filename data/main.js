a448525dd3ee329783cbc637de21fbc8d788c154
Huluti <hugo.posnic@protonmail.com>
2017-01-29 16:48:55 +0100

    Start adding filters

--- data/main.js
+++ data/main.js
@@ -1,8 +1,40 @@
 $(function () {
+    /*
+    FUNCTIONS
+    */
+    function updateList() {
+        // Update list of mirrors
+        $("#mirrors tr").each(function() {
+            var protocol = $("td:eq(2)", this).text();
+            if(protocol) {
+                if(!filters[protocol]) {
+                    $(this).hide();
+                }
+                else {
+                    $(this).show();
+                }
+            }
+        });
+    }
+
+    /*
+    MAIN
+    */
     // Enable tooltips
     $('[data-toggle="tooltip"]').tooltip()
     // Enable table sorting
     $('#mirrors').tablesorter({
         sortList: [[1, 0]]
+    });
+    // Filters
+    var filters = {
+        "http": true,
+        "https": true,
+        "ftp": true
+    }
+    $('input[type="checkbox"]').change(function() {
+        var filter = $(this).parent().find(".custom-control-description").text().toLowerCase();
+        filters[filter] = $(this).is(':checked');
+        updateList();
     });
 });
