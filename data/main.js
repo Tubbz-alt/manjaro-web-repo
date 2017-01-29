d2c4e35eba2193f4a9e2f17dff83a99210856241
Huluti <hugo.posnic@protonmail.com>
2017-01-29 17:47:38 +0100

    Add country filter

--- data/main.js
+++ data/main.js
@@ -2,8 +2,19 @@ $(function () {
     /*
     FUNCTIONS
     */
-    function updateList() {
-        // Update list of mirrors
+    function applyCountry() {
+        $("#mirrors tr").each(function() {
+            var country = $("td:eq(1)", this).text();
+            if(country) {
+                if(filters["country"] != "all") {
+                    filters["country"] != country ? $(this).hide() : $(this).show();
+                }
+                else
+                    $(this).show();
+            }
+        });
+    }
+    function applyProtocol() {
         $("#mirrors tr").each(function() {
             var protocol = $("td:eq(2)", this).text();
             if(protocol)
@@ -22,13 +33,20 @@ $(function () {
     });
     // Filters
     var filters = {
+        "country": "all",
         "http": true,
         "https": true,
         "ftp": true
     }
+
+    $("#country-select").change(function() {
+        var filter = $(this).find("option:selected").text();
+        filters["country"] = (filter == "All countries") ? "all" : filter;
+        applyCountry();
+    });
     $('input[type="checkbox"]').change(function() {
         var filter = $(this).parent().find(".custom-control-description").text().toLowerCase();
         filters[filter] = $(this).is(":checked");
-        updateList();
+        applyProtocol();
     });
 });
