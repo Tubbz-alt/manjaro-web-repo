8d9743bd765cea133d2ee3c67351a97111baf8e9
Huluti <hugo.posnic@protonmail.com>
2017-02-07 21:36:51 +0100

    JSLINT

--- data/js/main.js
+++ data/js/main.js
@@ -5,25 +5,26 @@ $(function () {
     // Enable table sorting
     $.tablesorter.addParser({
         id: "branch",
-        is: function(s, table, cell, $cell) {
+        is: function (s, table, cell, $cell) {
             return false;
         },
-        format: function(s, table, cell, cellIndex) {
-            if($(cell).html().includes("up"))
-                return 0
-            else if($(cell).html().includes("out"))
-                return 1
-            else
-                return 2
+        format: function (s, table, cell, cellIndex) {
+            if ($(cell).html().includes("up")) {
+                return 0;
+            } else if ($(cell).html().includes("out")) {
+                return 1;
+            } else {
+                return 2;
+            }
         },
         type: "numeric"
     });
     $.tablesorter.addParser({
         id: "sync",
-        is: function(s, table, cell, $cell) {
+        is: function (s, table, cell, $cell) {
             return false;
         },
-        format: function(s, table, cell, cellIndex) {
+        format: function (s, table, cell, cellIndex) {
             nb = $(cell).text().split(":");
             return +(nb[0] + "." + nb[1]);
         },
@@ -32,44 +33,42 @@ $(function () {
     $("#mirrors").tablesorter();
 
     // Enable tooltips
-    $('[data-toggle="tooltip"]').tooltip()
+    $("[data-toggle=\"tooltip\"]").tooltip();
 
     function updateList() {
         // Update list with filters
         var table = document.getElementById("mirrors");
         // Iterate table rows
-        for (var i = 1; i < table.rows.length; i++) {
-            var row = table.rows[i];
-            var country = table.rows[i].cells[1].textContent;
-            if(filters["country"] == "all") {
+        Object.keys(table.rows).forEach(function (key) {
+            var row = table.rows[key];
+            var country = row.cells[1].textContent;
+            if (filters.country === "all") {
                 country = "all";
             } else {
                 country = country.split(" ").join("_").toLowerCase();
             }
-            if(country) {
-                var protocols = table.rows[i].cells[2].textContent;
-                if(protocols) {
+            if (country) {
+                var protocols = row.cells[2].textContent;
+                if (protocols) {
                     protocols = protocols.split(", ");
                     // Check filters
-                    if(filters["http"] && protocols.includes("http")) {
+                    var condition = false;
+                    if (filters.http && protocols.includes("http")) {
                         condition = true;
-                    } else if(filters["https"] && protocols.includes("https")) {
+                    } else if (filters.https && protocols.includes("https")) {
                         condition = true;
-                    } else if(filters["ftp"] && protocols.includes("ftp")) {
+                    } else if (filters.ftp && protocols.includes("ftp")) {
                         condition = true;
-                    } else {
-                        condition = false;
                     }
-                    condition = condition && (filters["country"] == country);
-                    if(condition) {
+                    condition = condition && (filters.country === country);
+                    if (condition) {
                         row.style.display = "table-row";
-                    }
-                    else {
+                    } else {
                         row.style.display = "none";
                     }
                 }
             }
-        }
+        });
     }
 
     // Filters elts
@@ -81,22 +80,22 @@ $(function () {
         "https": document.getElementById("https-filter").checked,
         "ftp": document.getElementById("ftp-filter").checked,
         "country": country_f.value
-    }
+    };
 
     // Refresh list at start
     updateList();
 
     // Events on inputs
     var checkboxes = document.querySelectorAll(".custom-control-input");
-    for (var i = 0; i < checkboxes.length; i++) {
-        checkboxes[i].addEventListener("change", function() {
-            var protocol = this.id.split("-")[0];
-            filters[protocol] = this.checked;
+    Object.keys(checkboxes).forEach(function (key) {
+        checkboxes[key].addEventListener("change", function () {
+            var protocol = checkboxes[key].id.split("-")[0];
+            filters[protocol] = checkboxes[key].checked;
             updateList();
         });
-    }
-    country_f.addEventListener("change", function() {
-        filters["country"] = this.value;
+    });
+    country_f.addEventListener("change", function () {
+        filters.country = country_f.value;
         updateList();
     });
 });
