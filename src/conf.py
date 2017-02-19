fc864242b728b5b6be63ce0832a1ef532bbfb340
Hugo Posnic <hugo.posnic@protonmail.com>
2017-02-19 19:20:02 +0100

    Add log functionality

--- src/conf.py
+++ src/conf.py
@@ -5,3 +5,4 @@ MIRRORS_URL = "http://repo.manjaro.org/mirrors.json"
 BRANCHES = ("stable", "testing", "unstable")
 ROOT_FOLDER = "/var/www/manjaro-web-repo/"
 OUTPUT_FOLDER = "docs/"
+LOGS_FOLDER = "logs/"
--- src/main.py
+++ src/main.py
@@ -3,31 +3,33 @@
 import json
 import urllib.request
 import time
+import socket
 
 from collections import OrderedDict
-from conf import MIRRORS_URL, BRANCHES
 from mirror import Mirror
 from builder import Builder
-from helpers import close
+from logger import Logger
+from conf import MIRRORS_URL, BRANCHES
 
 
 class StatusChecker():
     """Launch of actions"""
 
     def __init__(self):
+        self.logger = Logger()
         self.mirrors = list()
         self.hashes = list()
         self.states = list()
+        self.logger.info("Manjaro-web-repo is starting")
 
     def get_mirrors(self):
         """Get list of mirrors"""
         try:
-            with urllib.request.urlopen(MIRRORS_URL) as mirrors_file:
+            with urllib.request.urlopen(MIRRORS_URL, timeout=10) as mirrors_file:
                 self.mirrors = json.loads(mirrors_file.read().decode("utf-8"),
                                           object_pairs_hook=OrderedDict)
-        except urllib.error.URLError:
-            print("Error: can't fetch list of mirrors.")
-            close()
+        except urllib.error.URLError as e:
+            self.logger.error("can't fetch list of mirrors", e)
 
     def get_hashes(self):
         """Get last hashes"""
@@ -41,13 +43,13 @@ class StatusChecker():
             except OSError:
                 pass
         if len(self.hashes) < len(BRANCHES):
-            print("Error: can't fetch last hashes.")
-            close()
+            self.logger.error("can't fetch last hashes")
 
     def check_mirrors(self):
         """Check each mirror"""
-        for mirror in self.mirrors:
-            print("{}: {}".format(mirror["country"], mirror["url"]))
+        nb = len(self.mirrors)
+        for i, mirror in enumerate(self.mirrors):
+            print("({}/{}): {}".format(i + 1, nb, mirror["url"]))
             mirror = Mirror(mirror)
             mirror.get_state_file()
             mirror.read_state_file(self.hashes)
@@ -59,11 +61,9 @@ class StatusChecker():
                 "branches": mirror.branches
             }
             self.states.append(mirror_status)
+        self.logger.info("{} mirror(s) added".format(len(self.states)))
 
 if __name__ == "__main__":
-    print("--------------------------------")
-    print("----Manjaro mirror's tracker----")
-    print("--------------------------------")
     status_checker = StatusChecker()
     status_checker.get_mirrors()
     status_checker.get_hashes()
@@ -71,3 +71,4 @@ if __name__ == "__main__":
     builder = Builder(status_checker.states)
     builder.write_json_output()
     builder.write_html_output()
+    status_checker.logger.close()
